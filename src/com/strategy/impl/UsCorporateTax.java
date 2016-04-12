a6b173ab0489b04c4d8dc95c440f3f5bd259c8bc
jet <jetison85@gmail.com>
2016-04-12 12:20:54 +0800

    context

---
+++ src/com/strategy/impl/UsCorporateTax.java
@@ -0,0 +1,15 @@
+package com.strategy.impl;
+
+import java.math.BigDecimal;
+
+import com.strategy.computeTax;
+
+public class UsCorporateTax implements computeTax {
+
+	@Override
+	public BigDecimal compute(BigDecimal profit, BigDecimal rate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
